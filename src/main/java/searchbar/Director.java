d8ef2275a21d6c1b70c478d97d615124bb80a47d
Sandra Molina <fhs42279@fh-salzburg.ac.at>
2021-12-05 11:10:50 -0500

    director

--- src/main/java/searchbar/Director.java
+++ src/main/java/searchbar/Director.java
@@ -1,2 +1,65 @@
 package searchbar;
 
+
+public class Director {
+
+	private int id;
+	private String name;
+	private int age;
+	private String nationality;
+	
+	public Director() 
+	{
+		this(0, "", 0, "");
+	}
+	
+	public Director(String name, int age, String nationality) 
+	{
+		this(0, name, age, nationality);
+	}
+	
+	public Director(int id, String name, int age, String nationality) 
+	{
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.nationality = nationality;
+	}
+
+	public int getId() 
+	{
+		return id;
+	}
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public int getAge() 
+	{
+		return age;
+	}
+	public void setAge(int age) 
+	{
+		this.age = age;
+	}
+
+	public String getNationality() 
+	{
+		return nationality;
+	}
+	public void setNationality(String nationality) 
+	{
+		this.nationality = nationality;
+	}
+	
+}
